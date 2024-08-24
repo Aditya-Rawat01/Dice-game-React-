36c440bef6cfc4b8851c7ecc0994f98809ce7c97
Aditya Rawat <adityarawat240@gmail.com>
2024-08-24 10:50:58 +0530

    another one

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://aditya-rawat01.github.io/Dice-game-React-/'
+  base:'/Dice-game-React-/'
 })
