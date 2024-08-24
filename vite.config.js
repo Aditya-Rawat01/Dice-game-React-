5cb006d0222ed23dde8e6e2aa27f071eadd36628
Aditya Rawat <adityarawat240@gmail.com>
2024-08-24 10:37:11 +0530

    Configuring github for react projects

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'https://aditya-rawat01.github.io/Dice-game-React-/'
 })
